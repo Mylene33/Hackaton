2a0f76703d5ca5007362f54174fb4bab2c8ff6f3
Tom Serrell <tomeo.serrell@gmail.com>
2021-02-13 21:36:29 +0100

    add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from './NavBar/logo.png'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer'>
+            <img className='logo-footer'src={logo}/>
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SideBar from '../components/SideBar'
 import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 
 const Contact = () => {
@@ -18,7 +19,8 @@ const Contact = () => {
             <NavBar toggle={toggle}/> 
 
             <h2 className='contact'>If you have any question or the intention to be part of the RAN'DON team, please contact us. Cheers ! </h2>
-            <a className='link' href="mailto:info@randon.com">info@randon.com</a>            
+            <a className='link' href="mailto:info@randon.com">info@randon.com</a> 
+            <Footer/>           
         </div>
     )
 }
