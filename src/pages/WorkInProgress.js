6d5d2e60e64aa3d5f6b9917580aa7ca4e2f95d1a
Yousra Miloudi <miloudi112@hotmail.com>
2021-02-16 11:18:09 +0100

    Lottie workinprogress

--- src/pages/WorkInProgress.js
+++ src/pages/WorkInProgress.js
@@ -3,6 +3,18 @@ import SideBar from '../components/SideBar'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
+import Lottie from 'react-lottie'
+import animationData from '../underconstruction.json'
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
 const WorkInProgress = () => {
 
     const [isOpen, setIsOpen] = useState(false)
@@ -18,6 +30,12 @@ const WorkInProgress = () => {
             <div className='container-wip'>
                 <h1 className='title-wip'>Oops 👼  this page is currently under construction</h1>
                 <h2 className='subtitle-wip'>Please come back later</h2>
+
+                <Lottie 
+                  options={defaultOptions}
+                  height={350}
+                  width={350}
+                  />
             </div>
             <Footer />
         </>
