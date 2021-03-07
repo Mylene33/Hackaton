8a987fe06d787eb4baa2e68abee90a6d02ac45a1
Mylene <mylene.freelance33@gmail.com>
2021-03-07 15:56:27 +0100

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='footer'>
-            <p className='copyright'>© Mylène, Yousra and Tom - RANDON </p>
+            <p className='copyright'>© Mylène, Yousra and Tom - RANDON 2021 </p>
         </div>
     )
 }
